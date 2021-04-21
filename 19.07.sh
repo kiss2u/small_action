cfac559947d4023f1e1845172899eb55fa700b52
kenzok8 <kenzok8@gmail.com>
2021-04-21 16:53:08 +0800

    int

--- 19.07.sh
+++ 19.07.sh
@@ -17,7 +17,7 @@ git clone https://github.com/small-5/luci-app-xlnetacc
 git clone https://github.com/project-lede/luci-app-godproxy
 git clone https://github.com/sirpdboy/luci-app-advanced
 git clone https://github.com/walkingsky/luci-wifidog luci-app-wifidog
-svn co https://github.com/garypang13/openwrt-packages/trunk/luci-app-gpsysupgrade
+svn co https://github.com/kenzok78/my-packages/trunk/luci-app-gpsysupgrade
 svn co https://github.com/garypang13/openwrt-packages/trunk/gost
 svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash
 svn co https://github.com/frainzy1477/luci-app-clash/trunk ./luci-app-clash
